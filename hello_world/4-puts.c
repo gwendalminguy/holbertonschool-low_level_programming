0f5fe094fb1bd9ee8cb144f7b029b1fd8975e6c8
gwendalminguy <gwendalminguy@outlook.fr>
2025-02-13 20:16:00 +0100

    Corrected expected output

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -3,7 +3,7 @@
 /* prints text */
 int main(void)
 {
-char text[] = "\"Programming is like building a multilingual puzzle\n";
+char text[] = "\"Programming is like building a multilingual puzzle";
 
 puts(text);
 
