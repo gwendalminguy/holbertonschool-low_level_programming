5c49059549368a5d180a5ecfa64d6efc5cfdddc5
gwendalminguy <gwendalminguy@outlook.fr>
2025-02-17 15:57:35 +0100

    Created function getting the last digit of n

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Gets the last digit of an integer
+ * @n: Number
+ *
+ * Return: Last digit of n
+ */
+int print_last_digit(int n)
+{
+	return (_abs(n % 10));
+}
