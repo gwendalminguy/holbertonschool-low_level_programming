7e9df23fa0e526d902e978655233d243de431020
gwendalminguy <gwendalminguy@outlook.fr>
2025-02-17 14:27:11 +0100

    Created function checking if character is a letter

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isalpha - Checks if a character is a letter
+ * @c: Character
+ *
+ * Return: 0 if false ; 1 if true
+ */
+int _islower(int c)
+{
+	if ((97 <= c && 122 >= c) || (65 <= c && 90 >= c))
+		return (1);
+	else
+		return (0);
+}
