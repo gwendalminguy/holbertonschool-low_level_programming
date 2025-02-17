8f744e2923301991b978e718bedf9f0ae368c42c
gwendalminguy <gwendalminguy@outlook.fr>
2025-02-17 14:01:47 +0100

    Updated with betty coding style

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * islower - Checks if a character is lowercase
+ * _islower - Checks if a character is lowercase
  * @c: Character
  *
  * Return: 0 if false ; 1 if true
  */
 int _islower(int c)
 {
-	if (97 <= c && c <= 122)
+	if (97 <= c && 122 >= c)
 		return (1);
 	else
 		return (0);
