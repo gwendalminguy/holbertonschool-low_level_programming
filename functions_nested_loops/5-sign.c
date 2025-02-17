f6131bfbc70d88ee0a1e9e6e27fc124331687621
gwendalminguy <gwendalminguy@outlook.fr>
2025-02-17 15:17:17 +0100

    Changed function description

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -4,16 +4,16 @@
  * print_sign - Prints the sign of a number
  * @n: Number
  *
- * Return: 0 if zero ; 1 if positive ; -1 if negative
+ * Return: 0 if zero / 1 if positive / -1 if negative
  */
 int print_sign(int n)
 {
-	if (0 < n)
+	if (n > 0)
 	{
 		_putchar(43);
 		return (1);
 	}
-	else if (0 > n)
+	else if (n < 0)
 	{
 		_putchar(45);
 		return (-1);
