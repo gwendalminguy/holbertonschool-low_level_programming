f4f291b1c083a5caae47e6cb99f967a30a1f2caa
gwendalminguy <gwendalminguy@outlook.fr>
2025-02-18 09:06:57 +0100

    Created function returning the sum of two numbers

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Gets the last digit of an integer
+ * @m: First number
+ * @n: Second number
+ *
+ * Return: Sum of m & n
+ */
+int add(int m, int n)
+{
+	return (m + n);
+}
