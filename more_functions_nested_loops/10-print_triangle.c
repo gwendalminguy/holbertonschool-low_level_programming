ba286a5cb074afe9fced62942fdbd5cbed6bbfb5
gwendalminguy <gwendalminguy@outlook.fr>
2025-02-24 13:30:02 +0100

    Created function printing triangle of any given size

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_triangle - Prints a triangle with '#' of any given size
+ */
+void print_triangle(int size)
+{
+	int i;
+	int space;
+	int symbol;
+
+	if (size > 0)
+	{
+		for (i = 1 ; i <= size ; i++)
+		{
+			for (space = 0 ; space <= (size - i) ; space++)
+			{
+				_putchar(' ');
+			}
+
+			for (symbol = 1 ; symbol <= i ; symbol++)
+			{
+				_putchar('#');
+			}
+
+			_putchar('\n');
+		}
+	}
+
+	else
+		_putchar('\n');
+}
