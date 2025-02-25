6b9e645e00af12c05e1f05b0a6e4e1f91de3173a
gwendalminguy <gwendalminguy@outlook.fr>
2025-02-25 13:58:56 +0100

    Changed function to replace repetitive instructions with a loop

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -6,39 +6,49 @@
  */
 void print_number(int n)
 {
-	int milliers;
-	int centaines;
-	int dizaines;
+	int length;
+	int digit;
+	int power;
+	int i;
+	int j;
 
 	if (n < 0)
 	{
 		_putchar(45);
 		n = -n;
 	}
-	if (n >= 1000)
-	{
-		if (n % 10000 >= 1000)
-			milliers = (n / 1000) % 1000;
-		else
-			milliers = 0;
-		_putchar('0' + milliers);
-	}
-	if (n >= 100)
-	{
-		if (n % 1000 >= 100)
-			centaines = (n / 100) % 100;
-		else
-			centaines = 0;
-		_putchar('0' + centaines);
-	}
-	if (n >= 10)
-	{
-		if (n % 100 >= 10)
-			dizaines = (n / 10) % 10;
-		else
-			dizaines = 0;
-		_putchar('0' + dizaines);
-	}
 
-	_putchar('0' + (n % 10));
+	if (n >= 1000000000)
+		length = 10;
+	else if (n >= 100000000)
+		length = 9;
+	else if (n >= 10000000)
+		length = 8;
+	else if (n >= 1000000)
+		length = 7;
+	else if (n >= 100000)
+		length = 6;
+	else if (n >= 10000)
+		length = 5;
+	else if (n >= 1000)
+		length = 4;
+	else if (n >= 100)
+		length = 3;
+	else if (n >= 10)
+		length = 2;
+	else
+		length = 1;
+
+	for (i = length ; i > 0 ; i--)
+	{
+		power = 1;
+		for (j = 0 ; j < i - 1 ; j++)
+			power *= 10;
+
+		if (n % (power * 10) >= power)
+			digit = (n / power) % 10;
+		else
+			digit = 0;
+		_putchar('0' + digit);
+	}
 }
