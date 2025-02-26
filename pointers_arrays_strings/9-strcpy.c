6145e30dcb868c06d448ade8c9fed3812c8b56ea
gwendalminguy <gwendalminguy@outlook.fr>
2025-02-26 09:02:12 +0100

    Created function copying a string from src to dest

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include "2-strlen.c"
+
+/**
+ * _strcpy - copies a string from src to dest
+ * @dest: Destination
+ * @src: Source
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int length = _strlen(src);
+	int i;
+
+	for (i = 0 ; i < length + 1 ; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	return (dest);
+}
+
