a97a6584a14517f40c04159c1f68bb4b3aecbc16
gwendalminguy <gwendalminguy@outlook.fr>
2025-02-27 14:05:25 +0100

    Corrected case where n > length_src

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -11,8 +11,12 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
+	int length_src = _strlen(src);
 	int length_dest = _strlen(dest);
 	int i;
+
+	if (length_src < n)
+		n = length_src;
 
 	for (i = 0 ; i < n ; i++)
 		dest[length_dest + i] = src[i];
