e88e197b05b918896ebb3f7a4a202516feebc43a
gwendalminguy <gwendalminguy@outlook.fr>
2025-02-27 17:39:41 +0100

    Added (again) \0 at end of dest

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -12,9 +12,16 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
+	int j;
 
 	for (i = 0 ; i < n ; i++)
 		dest[i] = src[i];
+
+	if (_strlen(src) < n)
+	{
+		for (j = _strlen(src) ; j < n ; j++)
+			dest[j] = '\0';
+	}
 
 	return (dest);
 }
