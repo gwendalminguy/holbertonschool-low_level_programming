7d6022e3b7c1fd22a21958fca65d0ede48a80aed
gwendalminguy <gwendalminguy@outlook.fr>
2025-02-27 13:31:55 +0100

    Created function appending n first src bytes to dest

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include "2-strlen.c"
+
+/**
+ * _strncat - concatenates two strings by appending n first src bytes to dest
+ * @dest: String
+ * @src: String
+ * @n: Number
+ *
+ * Return: updated dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int length_src = _strlen(src);
+	int length_dest = _strlen(dest);
+	int i;
+
+	for (i = 0 ; i < n ; i++)
+		dest[length_dest + i] = src[i];
+
+	return (dest);
+}
+
