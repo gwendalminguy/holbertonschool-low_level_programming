810acb7e1d6a43d32b10f23213132df57e4c527f
gwendalminguy <gwendalminguy@outlook.fr>
2025-02-27 16:49:44 +0100

    Added \0 in dest

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -13,8 +13,13 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0 ; i < n ; i++)
-		dest[i] = src[i];
+	for (i = 0 ; i <= n ; i++)
+	{
+		if (i != n)
+			dest[i] = src[i];
+		else
+			dest[i] = '\0';
+	}
 
 	return (dest);
 }
