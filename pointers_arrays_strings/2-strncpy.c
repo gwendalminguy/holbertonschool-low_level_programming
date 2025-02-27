aed73a63d9f9cdc5b0178a56b5876f470de1c0ee
gwendalminguy <gwendalminguy@outlook.fr>
2025-02-27 16:52:15 +0100

    Removed \0 in dest

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -13,13 +13,8 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0 ; i <= n ; i++)
-	{
-		if (i != n)
-			dest[i] = src[i];
-		else
-			dest[i] = '\0';
-	}
+	for (i = 0 ; i < n ; i++)
+		dest[i] = src[i];
 
 	return (dest);
 }
