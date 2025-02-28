57205d92c8840a2ab3c799b2c6c9815b18d4aa82
gwendalminguy <gwendalminguy@outlook.fr>
2025-02-28 10:32:24 +0100

    Created function capitalizing words in string

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/*
+ * cap_string - capitalizes all words of a string
+ * @str: String
+ *
+ * Return: updated str
+ */
+char *cap_string(char *str)
+{
+	int i, j;
+	int symbol[14] = {9, '\n',  32, 33, 34, 40, 41, 44, 46, 58, 59, 63, 123, 125};
+
+	for (i = 0 ; str[i] != '\0' ; i++)
+	{
+		if (str[i] > 96 && str[i] < 123)
+		{
+			for (j = 0 ; j < 14 ; j++)
+			{
+				if (str[i - 1] == symbol[j])
+				{
+					str[i] -= 32;
+				}
+			}
+		}
+	}
+
+	return (str);
+}
+
