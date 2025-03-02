4ab9b4219f10d5a8cb12fcdaf72b50a51ab7a06b
gwendalminguy <gwendalminguy@outlook.fr>
2025-03-02 15:59:08 +0100

    Created function generating random valid password for 101-crackme file

---
+++ pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/** main - generates characters key whose sum equals 2772
+ *
+ * Return: string matching expectations
+ */
+int main(void)
+{
+	char key[64];
+	unsigned int num;
+	int sum, i, j = 1;
+
+	srand(time(0));
+
+	while (sum != 2772)
+	{
+		*key = '\0';
+		sum = 0;
+		i = 0;
+
+		while (sum < 2772)
+		{
+			num = rand() % 26 + 65;
+			key[i] = num;
+			/* printf("key[%d] = %c\n", i, key[i]); */
+			sum += num;
+			i++;
+		}
+
+		key[i] = '\0';
+		/* printf("Attempt %d: %s\n", j, key); */
+		j++;
+	}
+
+	printf("%s\n", key);
+
+	return (0);
+}
+
