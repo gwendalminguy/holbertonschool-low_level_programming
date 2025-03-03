64c092af3971a3a8ff6656e748ea93dfcc807f24
gwendalminguy <gwendalminguy@outlook.fr>
2025-03-03 09:34:19 +0100

    Corrected with betty coding style

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,5 +1,13 @@
 #include "main.h"
 
+/**
+ * _memset - fills n first bytes of memory pointed to by s with constant byte b
+ * @s: memory area
+ * @b: byte to fill s with
+ * @n: number of bytes to change
+ *
+ * Return: updated s
+ */
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
