29dde20d399db1eda750de3ac34cd29c71a86942
gwendalminguy <gwendalminguy@outlook.fr>
2025-03-04 10:05:40 +0100

    Created function printing a string with recursion

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/** _puts_recursion - prints a string with recursion
+ * @s: string to print
+ */
+void _puts_recursion(char *s)
+{
+	int i = 0;
+
+	if (s[i] != '\0')
+	{
+		_putchar(s[i]);
+		_puts_recursion(&s[i + 1]);
+	}
+	else
+		_putchar('\n');
+}
+
