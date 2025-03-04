ea8cc6464792748b392c0c6aecffa7f874b2cb67
gwendalminguy <gwendalminguy@outlook.fr>
2025-03-04 10:33:30 +0100

    Created function printing a string in reverse with recursion

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints a string in reverse with recursion 
+ * @s: string to print
+ */
+void _print_rev_recursion(char *s)
+{
+	int i = 0;
+
+	if (s[i] != '\0')
+	{
+		_print_rev_recursion(&s[i + 1]);
+		_putchar(s[i]);
+	}
+}
+
