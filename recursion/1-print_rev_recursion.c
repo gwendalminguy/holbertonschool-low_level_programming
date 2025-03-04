07638d8b797cc123e1f8f557a9d6735bc53da8f8
gwendalminguy <gwendalminguy@outlook.fr>
2025-03-04 10:35:06 +0100

    Corrected with betty coding style

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _print_rev_recursion - prints a string in reverse with recursion 
+ * _print_rev_recursion - prints a string in reverse with recursion
  * @s: string to print
  */
 void _print_rev_recursion(char *s)
