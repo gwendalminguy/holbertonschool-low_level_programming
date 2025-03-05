6e1d5b13cb987cd6f6c7f251707dc0761b7bf383
gwendalminguy <gwendalminguy@outlook.fr>
2025-03-05 20:21:38 +0100

    Created program printing the product of two integers

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the product of two numbers given as arguments
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	int num_a, num_b;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	else
+	{
+		num_a = atoi(argv[1]);
+		num_b = atoi(argv[2]);
+
+		printf("%d\n", num_a * num_b);
+
+		return (0);
+	}
+}
