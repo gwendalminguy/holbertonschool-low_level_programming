2422dd7dcf57077ea0c24b44e774166a72928c31
gwendalminguy <gwendalminguy@outlook.fr>
2025-03-09 11:20:26 +0100

    Corrected case where a matching sequence must be ignored because of wildcard

--- recursion/101-wildcmp.c
+++ recursion/101-wildcmp.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * wildcmp - compares two strings taking into account wildcards
+ * wildcmp - compares two strings using wildcards
  * @s1: first string
  * @s2: second strings
  *
@@ -13,7 +13,7 @@ int wildcmp(char *s1, char *s2)
 }
 
 /**
- * compare_strings - compares two strings taking into account wildcards
+ * compare_strings - compares two strings using wildcards
  * @s1: first string
  * @s2: second string
  * @i: index for s1
@@ -30,8 +30,12 @@ int compare_strings(char *s1, char *s2, int i, int j)
 		return (compare_strings(s1, s2, i, j + 1));
 
 	if (s1[i] == s2[j])
-		return (compare_strings(s1, s2, i + 1, j + 1));
-	else if (s2[j - 1] == '*' && s1[i] != '\0')
+	{
+		if (compare_strings(s1, s2, i + 1, j + 1) == 1)
+			return (1);
+	}
+	
+	if (s2[j - 1] == '*' && s1[i] != '\0')
 		return (compare_strings(s1, s2, i + 1, j));
 	else
 		return (0);
