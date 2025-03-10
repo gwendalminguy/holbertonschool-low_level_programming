91105e21ffe1b01f834d07605f32f5a1a5ffd794
gwendalminguy <gwendalminguy@outlook.fr>
2025-03-10 10:06:26 +0100

    Created function creating initialized array of given size

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - creates an array and initializes it
+ * @size: size of array
+ * @c: char to initialize array with
+ *
+ * Return: created array
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *array;
+	int i;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	array = malloc(sizeof(char) * size);
+
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0 ; i < size ; i++)
+	{
+		array[i] = c;
+	}
+
+	return (array);
+}
