be633dca09625b091ee0f5d543c5188d457e5a36
gwendalminguy <gwendalminguy@outlook.fr>
2025-03-10 10:41:24 +0100

    Created function duplicating a string of any size

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - duplicates a string of any size
+ * @str: string to duplicate
+ *
+ * Return: prointer to duplicated string
+ */
+char *_strdup(char *str)
+{
+	char *s;
+	int size = 0;
+	int i;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	while (str[size] != '\0')
+	{
+		size++;
+	}
+
+	s = malloc(sizeof(char) * size + 1);
+	
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0 ; i <= size ; i++)
+	{
+		if (i != size)
+			s[i] = str[i];
+		else
+			s[i] = '\0';
+	}
+
+	return (s);
+}
--- malloc_free/main.h
+++ malloc_free/main.h
@@ -3,5 +3,6 @@
 
 void _putchar();
 char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
 
 #endif
