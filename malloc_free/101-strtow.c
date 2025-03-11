9c2151c7e2719d5f7c5c26e3d5ca636a58e6ca58
gwendalminguy <gwendalminguy@outlook.fr>
2025-03-11 19:41:07 +0100

    Created function splitting a string into words

---
+++ malloc_free/101-strtow.c
@@ -0,0 +1,86 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * strtow - splits a string into words
+ * @str: string to split
+ *
+ * Return: array of strings
+ */
+char **strtow(char *str)
+{
+	char **array;
+	int i, j;
+	int index = 0;
+	int length = 0;
+	int total = 0;
+	int words = 0;
+	int characters = 0;
+
+	if (str == NULL || str == "")
+		return (NULL);
+
+	while (str[total] != '\0')
+		total++;
+
+	for (i = 0 ; i < total ; i++)
+	{
+		/* Counting number of words */
+		if (str[i] != ' ' && str[i] != '\0' && (i == 0 || str[i - 1] == ' '))
+			words++;
+
+		/* Counting number of characters */
+		if (str[i] != ' ' && str[i] != '\0')
+			characters++;
+	}
+
+	/* Allocating memory for the array */
+	array = malloc(sizeof(char *) * words);
+
+	if (array == NULL)
+		return (NULL);
+
+	for (i = 0 ; str[i] != '\0' ; i++)
+	{
+		/* Detecting the begining of a word */
+		if (str[i] != ' ' && (i == 0 || str[i - 1] == ' '))
+		{
+			j = 0;
+
+			/* Counting characters for the word */
+			while (str[i + j] != ' ' || str[i + j] == 0)
+				j++;
+
+			length = j;
+
+			/* Allocating memory for the word */
+			array[index] = malloc(sizeof(char) * (length + 1));
+
+			/* Freeing memory if malloc fails */
+			if (array[index] == NULL)
+			{
+				while (index >= 0)
+				{
+					index--;
+					free(array[index]);
+				}
+				free(array);
+				return (NULL);
+			}
+
+			j = 0;
+
+			/* Copying the word */
+			while (j < length)
+			{
+				array[index][j] = str[i + j];
+				j++;
+			}
+			
+			array[index][j] = '\0';
+			index++;
+		}
+	}
+
+	return (array);
+}
