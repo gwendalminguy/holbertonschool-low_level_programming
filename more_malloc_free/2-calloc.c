2ac455d259a765403c569a533dae6b64ba0d38a8
gwendalminguy <gwendalminguy@outlook.fr>
2025-03-11 12:38:49 +0100

    Created function allocating memory for an array of nmemb elements

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - allocates memory for an array of nmemb elements
+ * @nmemb: number of elements in array
+ * @size: size of each element
+ *
+ * Return: pointer to allocated memory
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *ptr;
+	int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	ptr = malloc(nmemb * size);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0 ; i < nmemb * size ; i++)
+		ptr[i] = 0;
+
+	return (ptr);
+}
