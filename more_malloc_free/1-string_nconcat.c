cc6945497be4eda7fa312885e6ff2fe0831a2c50
gwendalminguy <gwendalminguy@outlook.fr>
2025-03-11 11:08:39 +0100

    Corrected case where s1 or s2 is NULL

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -15,17 +15,17 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i = 0;
 	unsigned int j = 0;
 
-	while (s1[i] != '\0')
-		i++;
-
-	while (s2[j] != '\0')
-		j++;
-
 	if (s1 == NULL)
 		s1 = "";
 
 	if (s2 == NULL)
 		s2 = "";
+
+	while (s1[i] != '\0')
+		i++;
+
+	while (s2[j] != '\0')
+		j++;
 
 	if (n > j)
 		n = j;
