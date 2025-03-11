9e544b1b16f00e6c2b29595659ada4c0f425f175
gwendalminguy <gwendalminguy@outlook.fr>
2025-03-11 20:54:11 +0100

    Added comments for clarity

--- malloc_free/101-strtow.c
+++ malloc_free/101-strtow.c
@@ -10,39 +10,65 @@
 char **strtow(char *str)
 {
 	char **array;
-	int i, j, index = 0, length = 0, words = 0;
+	int i, j;
+	int index = 0;
+	int length = 0;
+	int words = 0;
 
 	if (str == NULL)
 		return (NULL);
+
 	while (str[length] != '\0')
 		length++;
+
 	for (i = 0 ; i < length ; i++)
 	{
+		/* Counting number of words */
 		if (str[i] != ' ' && str[i] != '\0' && (i == 0 || str[i - 1] == ' '))
 			words++;
 	}
+
 	if (words == 0)
 		return (NULL);
+
+	/* Allocating memory for the array */
 	array = malloc(sizeof(char *) * (words + 1));
+
 	if (array == NULL)
 		return (NULL);
+
 	for (i = 0 ; str[i] != '\0' ; i++)
 	{
+		/* Detecting the begining of a word */
 		if (str[i] != ' ' && (i == 0 || str[i - 1] == ' '))
 		{
+			/* Counting characters for the word */
 			length = 0;
 			while (str[i + length] != ' ' && str[i + length] != '\0')
 				length++;
+
+			/* Allocating memory for the word */
 			array[index] = malloc(sizeof(char) * (length + 1));
+
+			/* Freeing memory if malloc fails */
 			if (array[index] == NULL)
 			{
-				for (; index >= 0 ; index--)
+				while (index >= 0)
+				{
+					index--;
 					free(array[index]);
+				}
 				free(array);
 				return (NULL);
 			}
-			for (j = 0 ; j < length ; j++)
+
+			/* Copying the word */
+			j = 0;
+			while (j < length)
+			{
 				array[index][j] = str[i + j];
+				j++;
+			}
 			array[index][j] = '\0';
 			index++;
 		}
