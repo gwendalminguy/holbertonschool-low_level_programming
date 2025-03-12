8a058aa50b35e35574df98988145c200e2f63176
gwendalminguy <gwendalminguy@outlook.fr>
2025-03-12 16:04:19 +0100

    Changed name and owner to be stored as copies

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -12,6 +12,7 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new;
+	int i, length;
 
 	new = malloc(sizeof(dog_t));
 
@@ -21,9 +22,31 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 
-	new->name = name;
+	length = 0;
+
+	while (name[length] != '\0')
+		length++;
+
+	new->name = malloc(sizeof(char) * (length + 1));
+
+	for (i = 0 ; i < length ; i++)
+		new->name[i] = name[i];
+
+	new->name[i] = '\0';
+
 	new->age = age;
-	new->owner = owner;
+
+	length = 0;
+
+	while (owner[length] != '\0')
+		length++;
+
+	new->owner = malloc(sizeof(char) * (length + 1));
+
+	for (i = 0 ; i < length ; i++)
+		new->owner[i] = owner[i];
+
+	new->owner[i] = '\0';
 
 	return (new);
 }
