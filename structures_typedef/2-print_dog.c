8d862d177d844213fe7c295517ed36b726554111
gwendalminguy <gwendalminguy@outlook.fr>
2025-03-12 10:09:51 +0100

    Corrected case where name, age or owner is NULL

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -3,11 +3,23 @@
 
 /**
  * print_dog - prints a struct dog
- * d: pointer to dog
+ * @d: pointer to dog
  */
 void print_dog(struct dog *d)
 {
-	printf("Name: %s\n", d->name);
-	printf("Age: %f\n", d->age);
-	printf("Owner: %s\n", d->owner);
+	if (d != NULL)
+	{
+		if (d->name != NULL)
+			printf("Name: %s\n", d->name);
+		else
+			printf("Name: (nil)\n");
+		if (d->age != NULL)
+			printf("Age: %f\n", d->age);
+		else
+			printf("Age: (nil)\n");
+		if (d->owner != NULL)
+			printf("Owner: %s\n", d->owner);
+		else
+			printf("Owner: (nil)\n");
+	}
 }
