d3048e93c92b99f119f2ee1270fac355cb56b132
gwendalminguy <gwendalminguy@outlook.fr>
2025-03-18 16:22:06 +0100

    Updated header file

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,8 +1,28 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 
+#include <stdarg.h>
+
+/**
+ * struct type - ...
+ *
+ * @letter: given char
+ * @type: matching type
+ */
+typedef struct type
+{
+        char letter;
+        void (*f)(va_list);
+} type_t;
+
+void print_char(va_list args);
+void print_int(va_list args);
+void print_float(va_list args);
+void print_string(va_list args);
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 
 #endif
