9b7394871414b6e55f8834527861a6d5e2c49559
gwendalminguy <gwendalminguy@outlook.fr>
2025-03-18 23:01:45 +0100

    Corrected functions return types

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -2,6 +2,7 @@
 #define VARIADIC_FUNCTIONS_H
 
 #include <stdarg.h>
+#include <stdio.h>
 
 /**
  * struct type - ...
@@ -12,13 +13,13 @@
 typedef struct type
 {
         char letter;
-        va_list (*f)(va_list args);
+        void (*f)(va_list);
 } type_t;
 
-va_list print_char(va_list args);
-va_list print_int(va_list args);
-va_list print_float(va_list args);
-va_list print_string(va_list args);
+void print_char(va_list args);
+void print_int(va_list args);
+void print_float(va_list args);
+void print_string(va_list args);
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
