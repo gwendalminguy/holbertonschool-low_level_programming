8da67ad2469fd530c0ceb61997b7b800400155d8
gwendalminguy <gwendalminguy@outlook.fr>
2025-03-21 19:02:57 +0100

    Created header file

---
+++ numbers/numbers.h
@@ -0,0 +1,14 @@
+#ifndef NUMBERS_H
+#define NUMBERS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int is_perfect_number(unsigned long long int n);
+unsigned long long int add_divisors(unsigned long long int n);
+void print_perfect_numbers(unsigned long long int n);
+int is_triangular_number(unsigned long long n);
+void print_triangular_numbers(unsigned long long int n);
+
+#endif
