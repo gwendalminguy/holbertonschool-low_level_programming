eb43a9c8d52feaa21a6f547cd524288faae25826
gwendalminguy <gwendalminguy@outlook.fr>
2025-03-31 13:36:20 +0200

    Created function to free a singly linked list

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - frees a list
+ * @head: head of the list
+ */
+void free_list(list_t *head)
+{
+	list_t *current = head;
+	void *temp;
+
+	while (current->next)
+	{
+		/* Saving address of next element */
+		temp = current->next;
+
+		/* Freeing current element */
+		free(current->str);
+		free(current->next);
+		free(current);
+
+		/* Switching current element to next one */
+		current = temp;
+	}
+
+	/* Freeing last element */
+	free(current->str);
+	free(current);
+}
