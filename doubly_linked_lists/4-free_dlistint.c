554d71e33de8fec49991507d54e569f4bc8dd432
gwendalminguy <gwendalminguy@outlook.fr>
2025-03-31 20:20:32 +0200

    Created function freeing a doubly linked list

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a list
+ * @head: head of the list
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *current = head;
+	void *temp;
+
+	if (head == NULL)
+	{
+		printf("Freed !\n");
+		exit(0);
+	}
+
+	while (current->next)
+	{
+		/* Saving address of next element */
+		temp = current->next;
+
+		/* Freeing current element */
+		free(current);
+
+		/* Switching current element to next one */
+		current = temp;
+	}
+
+	/* Freeing last element */
+	free(current);
+}
