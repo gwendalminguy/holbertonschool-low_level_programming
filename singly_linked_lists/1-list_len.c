6a54b44030176ecb4a99164fc236f68915c6c218
gwendalminguy <gwendalminguy@outlook.fr>
2025-03-31 09:52:03 +0200

    Created function computing the length of a singly linked list

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * list_len - computes the length of a list
+ * @h: head of the list
+ *
+ * Return: number of elements
+ */
+size_t list_len(const list_t *h)
+{
+	size_t length = 0;
+
+	if (h == NULL)
+		return (0);
+
+	while (h->next)
+	{
+		h = h->next;
+		length++;
+	}
+
+	length++;
+
+	return (length);
+}
