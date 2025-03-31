b23ea02305287d2601ec356a8e1e3fff84a3dd6d
gwendalminguy <gwendalminguy@outlook.fr>
2025-03-31 14:50:25 +0200

    Corrected with betty coding style

--- singly_linked_lists/100-first.c
+++ singly_linked_lists/100-first.c
@@ -3,9 +3,10 @@
 /**
  * startup_print - prints a message before execution of main
  */
-__attribute__ ((constructor)) void startup_print(void)
+__attribute__ ((constructor))void startup_print(void)
 {
-	char *message = "You're beat! and yet, you must allow,\nI bore my house upon my back!\n";
+	char *message_a = "You're beat! and yet, you must allow,\n";
+	char *message_b = "I bore my house upon my back!\n";
 
-	printf("%s", message);
+	printf("%s%s", message_a, message_b);
 }
