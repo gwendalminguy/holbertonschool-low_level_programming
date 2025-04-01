3d922a71adb935a07ad77549c9d1e239f6677d58
gwendalminguy <gwendalminguy@outlook.fr>
2025-04-01 10:00:53 +0200

    Removed unused variable

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -13,7 +13,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	unsigned int i = 0;
 	dlistint_t *current = *h;
 	dlistint_t *new = NULL;
-	dlistint_t *temp = NULL;
 	
 	if (h == NULL)
 		return (NULL);
