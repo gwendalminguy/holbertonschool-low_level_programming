43955200010f5ab03977515ad67005a153e2c55a
gwendalminguy <gwendalminguy@outlook.fr>
2025-04-02 23:15:28 +0200

    Changed STDERR exit status on write failure

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -79,7 +79,7 @@ void cp(const char *src, const char *dest)
 		if (bytes_written == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", dest);
-			exit(99);
+			exit(91);
 		}
 
 		bytes_read = read(fd_src, buffer, 1024);
