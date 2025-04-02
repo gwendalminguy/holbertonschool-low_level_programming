b1a73efee685611cfc7c8fd5f9c23d0a2b9589e3
gwendalminguy <gwendalminguy@outlook.fr>
2025-04-02 17:59:49 +0200

    Changed case where text_content is NULL

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * append_text_to_file - ...
+ * append_text_to_file - appends text to a file
  * @filename: name of the file
  * @text_content: text to append to the file
  *
@@ -22,6 +22,10 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
+	if (text_content == NULL)
+		return (1);
+
+	/* Counting bytes in text_content */
 	while (text_content[bytes])
 		bytes++;
 
