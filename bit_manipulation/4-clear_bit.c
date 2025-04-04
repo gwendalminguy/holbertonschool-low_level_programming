b88120f1a144c4de569054e7d6079dae70f34bfa
gwendalminguy <gwendalminguy@outlook.fr>
2025-04-04 10:25:31 +0200

    Created function disabling the bit at a given index for a number

---
+++ bit_manipulation/4-clear_bit.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * clear_bit - sets the bit of a number to 0 at a given index
+ * @n: pointer to the number
+ * @index: index to enable the bit at
+ *
+ * Return: 1 if successful ; -1 otherwise
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int bit = 1;
+
+	/* Checking if index is out of range */
+	if (index >= 64)
+		return (-1);
+
+	/* Setting the bit to the right position */
+	bit = bit << index;
+
+	/* Reversing the bit */
+	bit = ~ bit;
+
+	/* Disabling the bit in n */
+	*n = *n & bit;
+
+	return (1);
+}
