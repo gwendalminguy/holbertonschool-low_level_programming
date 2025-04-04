9dfbdbdf47177c562fef6c79af3d181f2c38a767
gwendalminguy <gwendalminguy@outlook.fr>
2025-04-04 18:27:12 +0200

    Corrected with betty coding style

--- doubly_linked_lists/103-keygen.c
+++ doubly_linked_lists/103-keygen.c
@@ -3,7 +3,8 @@
 #include <string.h>
 #include <time.h>
 
-/** main - generates a six characters key from a given username
+/**
+ * main - generates a six characters key from a given username
  * @argc: number of arguments
  * @argv: arguments
  *
@@ -13,63 +14,39 @@ int main(int argc, char **argv)
 {
 	char key[6];
 	char *user;
-	char *str = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
+	char *str;
 	int length = 0, sum = 0, product = 1, seed = 0, square = 0, random = 0;
 	int i = 0, j = 0;
 
-	if (argc != 2)
-	{
-		printf("Usage: ./keygen [username]\n");
-		return (1);
-	}
-
+	str = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
 	user = argv[1];
 	length = strlen(user);
-
-	/* First character of the key */
 	i = (length ^ 59) & 63;
 	key[0] = str[i];
-
-	/* Second character of the key */
 	for (j = 0 ; j < length ; j++)
 		sum += user[j];
-
 	i = (sum ^ 79) & 63;
 	key[1] = str[i];
-
-	/* Third character of the key */
 	for (j = 0 ; j < length ; j++)
 		product *= user[j];
-
 	i = (product ^ 85) & 63;
 	key[2] = str[i];
-
-	/* Fourth character of the key */
 	for (j = 0 ; j < length ; j++)
 	{
 		if (user[j] > seed)
 			seed = user[j];
 	}
-
 	srand(seed ^ 14);
 	i = rand() & 63;
 	key[3] = str[i];
-
-	/* Fifth character of the key */
 	for (j = 0 ; j < length ; j++)
 		square += user[j] * user[j];
-	
 	i = (square ^ 239) & 63;
 	key[4] = str[i];
-
-	/* Sixth character of the key */
 	for (j = 0 ; user[0] > j ; j++)
 		random = rand();
-
 	i = (random ^ 229) & 63;
 	key[5] = str[i];
-
 	printf("%s", key);
-
 	return (0);
 }
