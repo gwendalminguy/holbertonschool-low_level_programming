c4ed003e2a2f188a38fb82c30f4f352b3da2d573
gwendalminguy <gwendalminguy@outlook.fr>
2025-04-04 09:08:41 +0200

    Created function getting the value of a bit at a given index

---
+++ bit_manipulation/2-get_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * get_bit - gets the value of a bit at a given index for a given number
+ * @n: number
+ * @index: index to get the bit at
+ *
+ * Return: value of the bit
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int result = 0;
+
+	if (index >= 64)
+		result = -1;
+	else if (n >> index & 1)
+		result = 1;
+
+		return (result);
+}
